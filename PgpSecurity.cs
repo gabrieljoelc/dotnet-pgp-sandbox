3afe0c2ccb5c5ff2170844bd4cda6ae08a3059bf
Gabriel Chaney <gabriel.chaney@gmail.com>
2020-07-10 12:05:57 -0700

    Create project to encrypt/decrypt with pgpcore lib

---
+++ PgpSecurity.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using PgpCore;
+
+namespace PgpSandbox
+{
+    public interface ISecretManager
+    {
+        SecurityKeySource AcquireKeys(string tenant);
+    }
+
+    public class SecurityKeySource
+    {
+        public string PrivateKeySource { get; set; }
+        public string PublicKeySource { get; set; }
+        public string PassPhrase { get; set; }
+    }
+
+    internal class SimpleSecretManager : ISecretManager
+    {
+        private SecurityKeySource Source { get; }
+
+        public SimpleSecretManager(IOptions<SecurityKeySource> option)
+        {
+            Source = option.Value;
+        }
+
+        public SecurityKeySource AcquireKeys(string tenant)
+        {
+            return Source;
+        }
+    }
+
+    internal class PgpService
+    {
+        private readonly PGP _pgp;
+        private readonly ISecretManager _secretManager;
+        private ILogger<PgpService> Log { get; }
+
+        public PgpService(ISecretManager secretManager, ILogger<PgpService> log)
+        {
+            _pgp = new PGP();
+            _secretManager = secretManager;
+            Log = log;
+        }
+
+        public string Decrypt(string tenant, string input, string output)
+        {
+            var securityKeySource = _secretManager.AcquireKeys(tenant);
+            var inputStream = File.Open(input, FileMode.Open, FileAccess.ReadWrite);
+            var outStream = new MemoryStream();
+            var privateKeyStream = new MemoryStream(Encoding.UTF8.GetBytes(Regex.Unescape(securityKeySource.PrivateKeySource)));
+            securityKeySource.PassPhrase = securityKeySource.PassPhrase == "0" ? null : securityKeySource.PassPhrase; // signify it's null/empty in Azure (we can't store null values)
+            //_pgp.DecryptStream(inputStream, outStream, privateKeyStream, securityKeySource.PassPhrase);
+            _pgp.DecryptStream(inputStream, outStream, privateKeyStream, "");
+            inputStream.Dispose();
+            using (var outputStream = File.Open(output, FileMode.Create, FileAccess.Write))
+            outStream.WriteTo(outputStream);
+            outStream.Dispose();
+            return output;
+        }
+
+        public string Encrypt(string tenant, string input, string output)
+        {
+            Log.LogInformation("input: {input}", input);
+            Log.LogInformation("output: {output}", output);
+            var securityKeySource = _secretManager.AcquireKeys(tenant);
+            _pgp.EncryptFile(input, output, securityKeySource.PublicKeySource);
+            return output;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.IO;
+
+namespace PgpSandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create service collection
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+
+            // create service provider
+            var serviceProvider = services.BuildServiceProvider();
+
+            // entry to run app
+            serviceProvider.GetRequiredService<App>().Run();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            // build config
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            services.AddLogging(builder =>
+            {
+                builder.AddConsole();
+                builder.AddDebug();
+            });
+            services.AddOptions();
+            services.Configure<SecurityKeySource>(configuration.GetSection(nameof(SecurityKeySource)));
+            //public string PrivateKeySource { get; set; }
+            //public string PublicKeySource { get; set; }
+            //public string PassPhrase { get; set; }
+            services.Configure<FilePathes>(configuration.GetSection(nameof(FilePathes)));
+            //public string Input { get; set; }
+            //public string Output { get; set; }
+
+            // add services:
+            services.AddTransient<ISecretManager, SimpleSecretManager>();
+            services.AddTransient<PgpService>();
+
+            // add app
+            services.AddTransient<App>();
+        }
+    }
+
+    public class FilePathes
+    {
+        public string Input { get; set; }
+        public string Output { get; set; }
+    }
+
+    internal class App
+    {
+        private FilePathes Pathes { get; }
+        private PgpService Service { get; }
+
+        public App(IOptions<FilePathes> options, PgpService service)
+        {
+            Pathes = options.Value;
+            Service = service;
+        }
+
+        public void Run()
+        {
+            Console.WriteLine($"Pathes.Input: {Pathes.Input}");
+            var input = Service.Encrypt(null, Pathes.Input, Pathes.Output);
+            Console.WriteLine($"encrypted input: {input}");
+            //var output = Service.Decrypt(null, input, Pathes.Output);
+            //Console.WriteLine($"decrypted output: {output}");
+        }
+    }
+}
